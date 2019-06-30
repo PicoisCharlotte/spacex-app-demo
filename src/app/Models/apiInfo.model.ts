e92ebba8980fd3dff24561a5626d36b262b74e14
LaureenMartina <L.martina@protonmail.com>
2019-06-30 14:44:13 +0200

    add new Model: LaunchPads, Info API and Company

---
+++ src/app/Models/apiInfo.model.ts
@@ -0,0 +1,9 @@
+export interface ApiInfo {
+    project_name: string;
+    version: string;
+    project_link: string;
+    docs: string;
+    organization: string;
+    organization_link: string;
+    description: string;
+}
